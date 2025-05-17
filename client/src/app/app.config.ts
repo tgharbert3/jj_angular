30fb3e3739ff09ef3d62ffd5518f18304fee077f
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-05-17 12:18:03 -0400

    added firebase access

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
+import { firebaseConfig } from './enviroments/firebaseConfig';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideStorage(() => getStorage()),
+  provideAuth(() => getAuth()),
+  ],
+
 };
