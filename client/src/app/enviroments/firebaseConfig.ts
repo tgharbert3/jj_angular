b991a4e736b112d8193b3621daf550862b41a080
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-05-17 12:06:02 -0400

    added firebase config

---
+++ client/src/app/enviroments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA5o9SGteIYDIVZKADZUBjDAp4vZlXbfJA",
+    authDomain: "jjangular-48d77.firebaseapp.com",
+    projectId: "jjangular-48d77",
+    storageBucket: "jjangular-48d77.firebasestorage.app",
+    messagingSenderId: "625508017172",
+    appId: "1:625508017172:web:511dcbf0026fff3a8e7301"
+};
