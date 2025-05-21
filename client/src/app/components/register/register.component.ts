a6a702252fe31948bb0215517e7b1ef0e7a1f02f
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-05-21 15:12:24 -0400

    added basic form layout and control for register page

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  private formBuilder = inject(FormBuilder);
+
+  registerForm = this.formBuilder.group({
+
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    password: [''],
+    confirmPassword: [''],
+  })
+
+  onSubmit() {
+    window.alert(JSON.stringify(this.registerForm.value))
+  }
 }
