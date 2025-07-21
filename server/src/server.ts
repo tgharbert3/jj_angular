68176e7eb064738e1fd4cd059099e394b1afa220
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-07-21 17:11:22 -0400

    server.ts creates https server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
 import config from "./config";
 import app from "./app";
+import https from 'https';
+import fs from 'fs';
+import path from "path";
 
-// const options = {
-//     key: fs.readFileSync('../../.keys/server.key'),
-//     cert: fs.readFileSync('../../.keys/server.cert'),
-// }
+const options = {
+    key: fs.readFileSync(path.join(__dirname, '..', '.keys', 'server.key')),
+    cert: fs.readFileSync(path.join(__dirname, '..', '.keys', 'server.cert')),
+}
 
-// https.createServer(options, app).listen(PORT, () => {
-//     console.log(`App listening on port ${PORT}`);
-// });
+https.createServer(options, app).listen(config.port, () => {
+    console.log(`App listening on port ${config.port}`);
+});
 
-app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-})
+// app.listen(config.port, () => {
+//     console.log(`Server running on port ${config.port}`);
+// })
