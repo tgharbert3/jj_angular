b0639935800bef411f274812e084564d88af02a0
Tyler Harbert <tyler.g.harbert@gmail.com>
2025-07-28 17:48:41 -0400

    refactored login to use postgres. Added integration test and it passed

--- server/src/services/login.service.ts
+++ server/src/services/login.service.ts
@@ -1,25 +1,42 @@
-import mongoose from 'mongoose';
-import userSchema from '../schema/user.schema';
-
-const userModel = mongoose.models.User || mongoose.model('User', userSchema);
+import { db } from '../config';
 
 /**
- * Fetches user from the db
+ * Fetches user from the postgres db
  * @param {string} email 
  * @returns User object or Null
  */
 export async function fetchUserByEmail(email: string) {
-
     try {
-        const user = await userModel.findOne({ email: email });
-        if (user) {
-            return user
-        } else {
-            console.error("No user found");
-            return null
-        }
+        const user = await db.one('SELECT * FROM users.jj_users WHERE email = $1', [email]);
+        return user;
     } catch (error) {
-        console.error("Error fetching user: ", error);
+        console.error("No user found");
         throw error;
     }
 }
+
+// import mongoose from 'mongoose';
+// import userSchema from '../schema/user.schema';
+
+// const userModel = mongoose.models.User || mongoose.model('User', userSchema);
+
+// /**
+//  * Fetches user from the db
+//  * @param {string} email 
+//  * @returns User object or Null
+//  */
+// export async function fetchUserByEmail(email: string) {
+
+//     try {
+//         const user = await userModel.findOne({ email: email });
+//         if (user) {
+//             return user
+//         } else {
+//             console.error("No user found");
+//             return null
+//         }
+//     } catch (error) {
+//         console.error("Error fetching user: ", error);
+//         throw error;
+//     }
+// }
--- server/src/tests/integration_tests/login.integration.test.ts
+++ server/src/tests/integration_tests/login.integration.test.ts
@@ -1,26 +1,38 @@
 // const https = require('https');
 // const fetch = require('node-fetch');
 
+import { fetchUserByEmail } from "../../services/login.service";
+import { db } from "../../config";
+
 // const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
-// describe('POST /login', () => {
-//     it.skip('should return the users email on valid login', async () => {
-//         const response = await fetch('https://hopper.cis.uncw.edu:5001/login', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body:
-//                 JSON.stringify({ email: "test@gmail.com", password: "Test1234!" }),
-//             agent: httpsAgent,
-//         });
+describe('POST /login', () => {
+    afterAll(async () => {
+        await db.$pool.end()
+    })
+    // it.skip('should return the users email on valid login', async () => {
+    //     const response = await fetch('https://hopper.cis.uncw.edu:5001/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body:
+    //             JSON.stringify({ email: "test@gmail.com", password: "Test1234!" }),
+    //         agent: httpsAgent,
+    //     });
 
-//         expect(response.status).toBe(200);
+    //     expect(response.status).toBe(200);
 
-//         const user = await response.json();
-//         expect(user.email).toBe("test@gmail.com")
+    //     const user = await response.json();
+    //     expect(user.email).toBe("test@gmail.com")
 
-//     }, 15000);
+    // }, 15000);
 
-    
-// });
+    it('Should return the user', async () => {
+        const response = await fetchUserByEmail('test');
+        console.log('Response', response);
+        expect(response).toBeTruthy();
+    })
+
+
+});
